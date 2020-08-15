53ee5d72d15b6237be0d0ed8ec974b5f50a7bb2c
Joshua Mclean <joshmcleana@gmail.com>
2020-08-14 22:01:30 -0700

    Switched to local devlopment url

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const BASE_URL = "https://module-3-vision-board-backend.herokuapp.com"
-// const BASE_URL = "http://localhost:3000"
+// const BASE_URL = "https://module-3-vision-board-backend.herokuapp.com"
+const BASE_URL = "http://localhost:3000"
 const USERS_URL = `${BASE_URL}/users`
 const BOARDS_URL = `${BASE_URL}/boards`
 const GOALS_URL = `${BASE_URL}/goals`
